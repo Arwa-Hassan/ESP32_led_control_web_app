2740c69afda064c40659d272f4aa60d6f3daf19b
Arwa <arwahassan969@gmail.com>
2026-02-21 17:27:46 +0200

    logic fix

--- app.py
+++ app.py
@@ -2,17 +2,25 @@ from flask import Flask, jsonify, render_template
 
 app=Flask(__name__)
 
+led_state= {"2":"0"}
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/on')
 def turn_on():
-    return jsonify({"2":"1"})
+    led_state["2"] = "1"
+    return "ON"
 
 @app.route('/off')
 def turn_off():
-    return jsonify({"2":"0"})
+    led_state["2"] = "0"
+    return "OFF"
+
+@app.route('/state')
+def state():
+    return jsonify(led_state)
 
 if __name__ == "__main__" :
     app.run() 
